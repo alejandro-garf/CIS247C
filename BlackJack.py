858b85bb2d6def7e9cd1f1700e8137fc65ed9851
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2024-09-30 22:31:21 -0700

    List Comprehension

--- BlackJack.py
+++ BlackJack.py
@@ -2,25 +2,12 @@
 
 import random
 
-deck = [] #defines a list
+deck = [str(i) for i in range(2, 11)] + ['J', 'Q', 'K', 'A']
 
-for i in range(2, 11): #This will make it go from 2 to 10 just like a deck of cards
-    deck.append(str(i)) #Adds a string to the list, needs to be string cause will be adding cards like "jacks" "Kings" etc.
-deck.append('J')
-deck.append('Q')
-deck.append('K')
-deck.append('A')
 
 deck *= 4 #Multiplies all the cards by 4, ignore Suit
 print("Number of cards in the deck: ", len(deck))
 print(deck)
 
-random.shuffle(deck)
-print(deck)
-
-top_card = deck.pop(0)
-print(top_card)
-
-print(deck)
 
 
