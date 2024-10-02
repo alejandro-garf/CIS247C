fef984a427ed833cf380cac83aab04a4a19ef09c
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2024-10-02 12:49:19 -0700

    Lists

--- Notes/datatypes2.py
+++ Notes/datatypes2.py
@@ -8,3 +8,35 @@ print(state)
 
 newState = state.replace("Ca", "Bz")
 print(newState)
+
+# String Operations: isDigit and isapha
+str = "Hello"
+print(str.isalpha())
+str = "Hello12"
+print(str.isalpha())
+
+str = "1234"
+print(str.isdigit())
+str = "123b4"
+print(str.isdigit())
+
+# Upper
+str = "Hello"
+print(str.upper())
+
+# Lower
+str = "HELLO"
+print(str.lower())
+
+# Lists
+L = [1, 'List', 23, "b"]
+L[1] = 54
+print(L)
+
+L = [1, 'List', 23, "b"]
+print(L)
+
+MyList = [1, 'List', 23, "b"]
+print(len(MyList))
+MyList.append("Sup Baby")
+print(len(MyList))
