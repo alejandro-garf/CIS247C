54a094f21559859483d22943ddc78efb85396828
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2024-10-02 15:30:29 -0700

    Function

---
+++ Notes/Lecture5.py
@@ -0,0 +1,5 @@
+# Mixing data types
+a = 5
+b = input("Enter a number: ")
+c = float(b) * a
+print(c)
---
+++ Notes/ifelifelse.py
@@ -0,0 +1,17 @@
+
+def compare(value):
+    try:
+        correct_value = value
+        guess = int(input("Guess a number between 1 and 10: "))
+        if guess < 1 or guess > 10:
+            raise ValueError("The guess must be between 1 and 10")
+        elif guess > correct_value:
+            print("The guess is too high, try again!")
+        elif guess < correct_value:
+            print("The guess is too low, try again!")
+        else:
+            print("The guess is correct! Congrats!")
+    except ValueError as ve:
+        print(ve)
+
+compare(5)
