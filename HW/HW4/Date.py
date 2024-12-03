62a01c84f2e348a1e52af6b1a5b0ad6851973535
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2024-12-02 22:25:09 -0800

    Date.py file

--- HW/HW4/Date.py
+++ HW/HW4/Date.py
@@ -1,48 +1,18 @@
+# Class that keeps track of dates
 class Date:
-    def __init__(self, day, month, year):
-        self._day = day
-        self._month = month
-        self._year = year
+    def __init__(self, d, m, y):
+        # These are instance variables that store the date info
+        self.day = d  # the day of the month
+        self.month = m  # which month it is
+        self.year = y  # what year it is
 
-    @property
-    def day(self):
-        return self._day
-
-    @day.setter
-    def day(self, value):
-        if 1 <= value <= 31:
-            self._day = value
+    # Function that checks if two dates are the same
+    def is_same_date(self, other_date):
+        if self.day == other_date.day and self.month == other_date.month and self.year == other_date.year:
+            return True
         else:
-            raise ValueError("Day must be between 1 and 31")
+            return False
 
-    @property
-    def month(self):
-        return self._month
-
-    @month.setter
-    def month(self, value):
-        if 1 <= value <= 12:
-            self._month = value
-        else:
-            raise ValueError("Month must be between 1 and 12")
-
-    @property
-    def year(self):
-        return self._year
-
-    @year.setter
-    def year(self, value):
-        if value >= 0:
-            self._year = value
-        else:
-            raise ValueError("Year cannot be negative")
-
-    def __str__(self):
-        return f"{self._month}/{self._day}/{self._year}"
-
-    def __eq__(self, other):
-        if isinstance(other, Date):
-            return (self._day == other.day and
-                    self._month == other.month and
-                    self._year == other.year)
-        return False
+    # Prints out the date
+    def print_date(self):
+        return str(self.month) + "/" + str(self.day) + "/" + str(self.year)
